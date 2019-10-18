8c260e0fb0792f0add047e5b5171997923a855fb
Jeff Swisher <jefftswisher@gmail.com>
2019-10-18 17:51:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,18 @@
 class Dog 
 
-
-
-
-
+  def name=(dog_name)
+    @dog_instance_name = dog_name
+  end 
+  
+  def name
+    @dog_instance_name
+  end 
+  
+  def breed=(dog_breed)
+    @dog_instance_breed = dog_breed
+  end
+  
+  def breed 
+    @dog_instance_breed
+  end 
 end 
